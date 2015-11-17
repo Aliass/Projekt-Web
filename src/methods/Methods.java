92ea6c80e2d32b206541c8565d7515283beb6120
Aliass <ligaofshadow@gmail.com>
2015-11-17 09:48:59 +0100

    Add Methods class

---
+++ src/methods/Methods.java
@@ -0,0 +1,21 @@
+package methods;
+
+public class Methods {
+
+	public int version;
+	
+	//Constructor
+	public Methods(){
+		
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+		
+	
+}
